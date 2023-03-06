bb79da1e091e90241279dcfc4418fea081ea5c92
Hassan Refaat <hassan.refaat.dev@gmail.com>
2023-03-06 21:15:38 +0200

    init commit

---
+++ src/main/java/io/nerd/springcicdapp/SpringCicdAppApplication.java
@@ -0,0 +1,13 @@
+package io.nerd.springcicdapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCicdAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCicdAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/nerd/springcicdapp/SpringCicdAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nerd.springcicdapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCicdAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
